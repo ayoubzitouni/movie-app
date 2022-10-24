f098fec8f67b8265265e99c5ecf1454670b81b4d
BuildTools <ayoubzitouni315@gmail.com>
2022-10-24 17:43:19 +0100

    0.5

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Nbar from'./components/Nvbar'
 import Hscreen from './components/Homescreen'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-
-
-
-
-
-
-
+import Data from './components/data';
+import { useState } from 'react';
 function App() {
+  const [film,setFilm]=useState(Data)
+  const addFilm=(newfilm)=>{
+    
+    setFilm([...film,newfilm])
+    console.log(film)
+    console.log(Data)
+
+  }
   return (
     <div className="App">
-      <Nbar/>
+      <Nbar addFilm={addFilm}/>
       <Hscreen className="homescreen"/>
       <MovieList className="movielist" />
     </div>
--- src/components/Nvbar.jsx
+++ src/components/Nvbar.jsx
@@ -1,21 +1,41 @@
 import React, { useState } from 'react'
 import { Button, Container, Modal, Navbar } from 'react-bootstrap'
-const Nvbar = () => {
+
+
+const Nvbar = ({addFilm}) => {
+
+
   const [show,setShow]=useState(false)
   const [fname,setFname]=useState('')
   const [desc,setDesc]=useState('')
   const [fpost,setFpost]=useState('')
-  const [frt,setFrt]=useState(0)
+  const [frt,setFrt]=useState("")
+
+
 const Toggle=()=>{
 setShow(!show);
 }
 const Check=()=>{
-if(fname!="" && desc!="" && frt!="" && fpost!=""){
-  console.log("jawek behi sahbi")
-}else {console.log("jawek mouch behi sahbi")}
-Toggle()
+if(fname!="" && desc!="" && /^\d+$/.test(frt) && /https?:\/\/.*\.(?:png|jpg)/.test(fpost)){
+  Add()
+  console.log(fpost)
+  Toggle()
+  setDesc("")
+  setFname("")
+  setFpost("")
+  setFrt("")
+  console.log("done")
+}else {console.log("not done")}
+
 }
 const Add=()=>{
+  let newfilm={
+    name:fname,
+    description:desc,
+    rate:frt,
+    posterUrl:fpost
+  }
+  addFilm(newfilm)
   
 }
 
@@ -27,8 +47,8 @@ const Add=()=>{
             Freeflix
           </Navbar.Brand>
           <Navbar.Brand>
-            <button onClick={Toggle}>Add a film
-            </button>
+            <Button onClick={Toggle}>Add a film
+            </Button>
             <Modal show={show} onHide={Toggle}>
               <Modal.Header closeButton>
               <Modal.Title>Modal heading</Modal.Title>
@@ -51,12 +71,8 @@ const Add=()=>{
                 <input type="search" name="frt" id="frt" onChange={(frt)=>setFrt(frt.target.value)}/>
               </Modal.Body>
               <Modal.Footer>
-                <Button variant="secondary" onClick={Toggle}>
-                  Close
-                </Button>
-                <Button variant="primary" onClick={Check}/>
-                  Add the film
-                </Buttton>
+                <Button variant="secondary" onClick={Toggle}>Close</Button>
+                <Button variant="primary" onClick={Check}>Add the film</Button>
               </Modal.Footer>
             </Modal>
             
